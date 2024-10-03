dd0c59098c6c1239d1a52e3147ebbfb5661f0676
B-Hoy <bradley.hoy@student.uts.edu.au>
2024-10-03 17:19:05 +1000

    confirm that azure picks up new tests

---
+++ src/test/java/uts/advsoft/TwoPlusTwoTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+class TwoPlusTwoTest{
+	@Test
+	void test_maths_works(){
+		assertTrue(2 + 2 == 4);
+	}
+}
